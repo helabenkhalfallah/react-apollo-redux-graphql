22d0311c2142286ac6de93e16fb5784365ad6e73
helabenkhalfallah <hela@upperskills.com>
2018-06-17 12:53:55 +0100

    [react-apollo-redux-graphql] update primary & secondary button theme

--- src/core/components/button/PrimaryButton.jsx
+++ src/core/components/button/PrimaryButton.jsx
@@ -1,17 +1,22 @@
 import styled from 'styled-components'
-import ThemeProvider from '../theme/Theme'
+// import ThemeProvider from '../theme/Theme'
 
 // setting for primary button
 // this button have not props
 // settings are controlled by : ThemePrimaryButton
 const PrimaryButton = styled.button`
   display: inline-block;
-  color: palevioletred;
+  background : #c60000;
+  color: white;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: none;
   border-radius: 3px;
+  outline-width: 0;
+  outline-style: none;
+  outline-color: none;
+  outline: none;
 `
 
 export default PrimaryButton
--- src/core/components/button/SecondaryButton.jsx
+++ src/core/components/button/SecondaryButton.jsx
@@ -1,17 +1,22 @@
 import styled from 'styled-components'
-import ThemeProvider from '../theme/Theme'
+// import ThemeProvider from '../theme/Theme'
 
 // setting for primary button
 // this button have not props
 // settings are controlled by ThemeSecondaryButton
 const SecondaryButton = styled.button`
   display: inline-block;
-  color: yellow;
+  background : white;
+  color: #c60000;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid #c60000;
   border-radius: 3px;
+  outline-width: 0;
+  outline-style: none;
+  outline-color: none;
+  outline: none;
 `
 
 export default SecondaryButton
